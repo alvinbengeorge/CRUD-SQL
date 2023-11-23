334b619b2c0eaee2a774297d9dcf89b2f8538fa6
Alvin Ben <69302420+alvinbengeorge@users.noreply.github.com>
2023-11-23 22:26:42 +0530

    feat: containerized sql

---
+++ sql/database_init.sql
@@ -0,0 +1,10 @@
+use db;
+
+CREATE TABLE log (
+    user varchar(10),
+    time int,
+    description varchar(100),
+    id int 
+)
+
+DESCRIBE log;
---
+++ src/run.sh
@@ -0,0 +1,2 @@
+sleep 10;
+uvicorn main:app --host=0.0.0.0 --port=8000;
